2ba8e4c0023d745629dadc200590ac58b1c08958
Oliver Bucher <7040739+Level8Broccoli@users.noreply.github.com>
2021-07-31 22:48:54 +0000

    added auth

--- src/app.jsx
+++ src/app.jsx
@@ -1,3 +1,27 @@
+import { Authentification } from "./components/Authentification";
+import { useEffect } from "preact/hooks";
+
 export function App(props) {
-  return <p>Hello Vite + Preact!</p>;
+  const OLYMP_HOST = "https://api.gildedernacht.ch/";
+
+  useEffect(async () => {
+    window.olymp = new Olymp({ server: OLYMP_HOST });
+  }, []);
+
+  return (
+    <>
+      <section className="section">
+        <div className="container">
+          <h1 className="title is-1">
+            Admin <span className="tag is-large is-dark">Gilde der Nacht</span>
+          </h1>
+        </div>
+      </section>
+      <section className="section">
+        <div className="container">
+          <Authentification host={OLYMP_HOST}></Authentification>
+        </div>
+      </section>
+    </>
+  );
 }
---
+++ src/components/Authentification.jsx
@@ -0,0 +1,74 @@
+import { useState } from "preact/hooks";
+
+export function Authentification(props) {
+  const [server, setServer] = useState(props.host);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [visible, setVisible] = useState(true);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    window.olymp = new Olymp({
+      server,
+      username,
+      password,
+    });
+  };
+
+  return (
+    <>
+      <h2 className="title is-2">
+        Authentifizierung
+        <button class="button is-small is-ghost" onClick={() => setVisible(!visible)}>
+          {visible ? "Zuklappen" : "Aufklappen"}
+        </button>
+      </h2>
+      {visible && (
+        <form action="#" onSubmit={handleSubmit}>
+          <div class="field">
+            <label class="label">Server</label>
+            <div class="control">
+              <input
+                class="input"
+                type="text"
+                value={server}
+                onInput={(e) => {
+                  setServer(e.target.value);
+                }}
+              />
+            </div>
+          </div>
+          <div class="field">
+            <label class="label">Benutzername</label>
+            <div class="control">
+              <input
+                class="input"
+                type="text"
+                value={username}
+                onInput={(e) => {
+                  setUsername(e.target.value);
+                }}
+              />
+            </div>
+          </div>
+          <div class="field">
+            <label class="label">Passwort</label>
+            <div class="control">
+              <input
+                class="input"
+                type="password"
+                value={password}
+                onInput={(e) => {
+                  setPassword(e.target.value);
+                }}
+              />
+            </div>
+          </div>
+          <button class="button is-primary" type="submit">
+            Authentifizieren
+          </button>
+        </form>
+      )}
+    </>
+  );
+}
