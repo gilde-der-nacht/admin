73e4a555d6b6bc50ebaf5c71b5b4a3bde2fe4e25
Oliver Bucher <7040739+Level8Broccoli@users.noreply.github.com>
2021-08-01 14:33:39 +0000

    add raw

--- src/components/EntryDetail.jsx
+++ src/components/EntryDetail.jsx
@@ -4,7 +4,9 @@ export function EntryDetail(props) {
       return (
         <div className="box">
           <h3 className="title is-3">{key}</h3>
-          {Object.entries(value).length === 0 ? (
+          {typeof value === "boolean" || typeof value === "number" ? (
+            value.toString()
+          ) : Object.entries(value).length === 0 ? (
             <code>empty/hidden</code>
           ) : typeof value === "object" ? (
             renderObject(value)
@@ -22,9 +24,13 @@ export function EntryDetail(props) {
       {Object.entries(props.entry).length === 0 ? (
         "Bitte wähle oben einen Eintrag aus."
       ) : (
-        <span>
-          Eintrag: <code>{props.entry.entryUid}</code>
-        </span>
+        <>
+          <p>
+            Eintrag: <code>{props.entry.entryUid}</code>
+          </p>
+          <p>Raw:</p>
+          <pre>{JSON.stringify(props.entry, null, 2)}</pre>
+        </>
       )}
       {renderObject(props.entry)}
     </>
